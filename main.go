4f3ff1a245cf12af8441868d44e3ed6f74177455
Tianon Gravi <admwiggin@gmail.com>
2014-05-12 11:54:39 -0600

    initial version

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+
+	"github.com/dotcloud/docker/pkg/user"
+)
+
+func main() {
+	log.SetFlags(0) // no timestamps on our logs
+
+	if len(os.Args) <= 2 {
+		log.Printf("Usage: %s user-spec command [args]", os.Args[0])
+		log.Printf("   ie: %s tianon bash", os.Args[0])
+		log.Printf("       %s nobody:root bash -c 'whoami && id'", os.Args[0])
+		log.Printf("       %s 1000:1 id", os.Args[0])
+		os.Exit(1)
+	}
+
+	uid, gid, suppGids, err := user.GetUserGroupSupplementary(os.Args[1], syscall.Getuid(), syscall.Getgid())
+	if err != nil {
+		log.Fatalf("error: failed parsing '%s': %v", os.Args[1], err)
+	}
+
+	if err := syscall.Setgroups(suppGids); err != nil {
+		log.Fatalf("error: failed to setgroups: %v", err)
+	}
+	if err := syscall.Setgid(gid); err != nil {
+		log.Fatalf("error: failed to setgid: %v", err)
+	}
+	if err := syscall.Setuid(uid); err != nil {
+		log.Fatalf("error: failed to setuid: %v", err)
+	}
+
+	name, err := exec.LookPath(os.Args[2])
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	err = syscall.Exec(name, os.Args[2:], os.Environ())
+	if err != nil {
+		panic(err)
+	}
+}
