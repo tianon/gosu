467c9206d4d64681eed2b8258ade19607b1ae063
Tianon Gravi <admwiggin@gmail.com>
2014-11-19 00:56:50 -0700

    Even better signing (ie, regenerate SHA256SUMS to include the SHA256SUMS of the *.asc files)

--- sign.sh
+++ sign.sh
@@ -5,7 +5,9 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 set -x
 rm -f gosu*.asc SHA256SUMS.asc
-for f in SHA256SUMS gosu*; do
+for f in gosu*; do
 	gpg --output "$f.asc" --detach-sign "$f"
 done
+sha256sum gosu* > SHA256SUMS
+gpg --output SHA256SUMS.asc --detach-sign SHA256SUMS
 ls -lFh gosu* SHA256SUMS*
