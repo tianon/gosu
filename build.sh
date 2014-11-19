2ae05680feb912af5d09310b86a8234bb1ce28e3
Tianon Gravi <admwiggin@gmail.com>
2014-11-19 00:52:30 -0700

    Better/safer build/sign

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 set -x
 docker pull $(awk '/^FROM\s/ { print /:/ ? $2 : $2":latest" }' Dockerfile)
 docker build -t gosu .
-rm -f gosu* SHA256SUMS
+rm -f gosu* SHA256SUMS*
 docker run --rm gosu bash -c 'cd /go/bin && tar -c gosu*' | tar -xv
 sha256sum gosu* > SHA256SUMS
 cat SHA256SUMS
-ls -lFh gosu* SHA256SUMS
+ls -lFh gosu* SHA256SUMS*
 ./gosu-amd64
--- sign.sh
+++ sign.sh
@@ -4,6 +4,8 @@ set -e
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 set -x
+rm -f gosu*.asc SHA256SUMS.asc
 for f in SHA256SUMS gosu*; do
 	gpg --output "$f.asc" --detach-sign "$f"
 done
+ls -lFh gosu* SHA256SUMS*
