14d1ff0e64951c31f2446be72f0698c2cdee74ff
Tianon Gravi <admwiggin@gmail.com>
2014-11-19 00:49:10 -0700

    Also generate SHA256SUMS on build

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 set -x
 docker build -t gosu .
-rm -f gosu*
+rm -f gosu* SHA256SUMS
 docker run --rm gosu bash -c 'cd /go/bin && tar -c gosu*' | tar -xv
-ls -lFh gosu*
+sha256sum gosu* > SHA256SUMS
+cat SHA256SUMS
+ls -lFh gosu* SHA256SUMS
 ./gosu-amd64
