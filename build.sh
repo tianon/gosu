eba2e8fc69dfabb1e0bd2d98506231958f0ca487
Tianon Gravi <admwiggin@gmail.com>
2016-01-08 17:03:11 -0800

    MOAR TESTING

--- build.sh
+++ build.sh
@@ -4,9 +4,12 @@ set -e
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 set -x
+
 docker build --pull -t gosu .
+
 rm -f gosu* SHA256SUMS*
 docker run --rm gosu bash -c 'cd /go/bin && tar -c gosu*' | tar -xv
 sha256sum gosu* | tee SHA256SUMS
 ls -lFh gosu* SHA256SUMS*
-./gosu-$(dpkg --print-architecture) || :
+
+"./gosu-$(dpkg --print-architecture)" || :
--- test.sh
+++ test.sh
@@ -9,11 +9,24 @@ usage() {
 gosu="$1"
 shift || { usage >&2; exit 1; }
 
-dir="$(mktemp -d -t gosu-test-XXXXXXXXXX)"
-trap "rm -rf '$dir'" EXIT
+trap '{ set +x; echo; echo FAILED; echo; } >&2' ERR
 
 set -x
-mkdir -p "$dir"
+
+dir="$(mktemp -d -t gosu-test-XXXXXXXXXX)"
+base="$(basename "$dir")"
+img="gosu-test:$base"
+trap "rm -rf '$dir'" EXIT
 cp Dockerfile.test "$dir/Dockerfile"
 cp "$gosu" "$dir/gosu"
-docker build "$dir"
+docker build -t "$img" "$dir"
+rm -rf "$dir"
+trap - EXIT
+
+trap "docker rm -f '$base' > /dev/null; docker rmi -f '$img' > /dev/null" EXIT
+
+docker run -d --name "$base" "$img" gosu root sleep 1000
+sleep 1 # give it plenty of time to get through "gosu" and into the "sleep"
+[ "$(docker top "$base" | wc -l)" = 2 ]
+# "docker top" should have only two lines
+# -- ps headers and a single line for the single process running in the container
