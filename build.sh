eaab61dc6320dbadc1f3772020841002eefd987b
Tianon Gravi <admwiggin@gmail.com>
2016-01-11 10:06:30 -0800

    Use "sh" instead of "bash" for extracting built binaries (especially important since we're on Alpine now and don't have bash)

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ set -x
 docker build --pull -t gosu .
 
 rm -f gosu* SHA256SUMS*
-docker run --rm gosu bash -c 'cd /go/bin && tar -c gosu*' | tar -xv
+docker run --rm gosu sh -c 'cd /go/bin && tar -c gosu*' | tar -xv
 sha256sum gosu* | tee SHA256SUMS
 ls -lFh gosu* SHA256SUMS*
 
