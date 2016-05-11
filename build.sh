aaa88b9e639044c532e196df408b924bea17ca0c
Tianon Gravi <admwiggin@gmail.com>
2016-05-10 21:48:26 -0700

    Add one more "file" invocation outside the build container

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ docker build --pull -t gosu .
 rm -f gosu* SHA256SUMS*
 docker run --rm gosu sh -c 'cd /go/bin && tar -c gosu*' | tar -xv
 sha256sum gosu* | tee SHA256SUMS
+file gosu*
 ls -lFh gosu* SHA256SUMS*
 
 "./gosu-$(dpkg --print-architecture)" || :
